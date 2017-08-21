cedcc09825acf7bdeb1f2ef2c945fc390029ac8a
Devon Worland <devonworland@Devon-Worlands-MacBook-Pro.local>
2017-08-21 00:57:20 -0500

    add wins, losses, reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,52 +5,97 @@ $(document).ready(function() {
 	var crystalValue2 = Math.floor(Math.random() * 12) + 1;
 	var crystalValue3 = Math.floor(Math.random() * 12) + 1;
 	var crystalValue4 = Math.floor(Math.random() * 12) + 1;
-	var totalCrystalValue = crystalValue1 + crystalValue2 + crystalValue3 + crystalValue4
 	var wins = 0
 	var losses = 0
+	var score = 0
 
 	for ( i = 19; i < 120; i++); {
-		console.log("check");
-		console.log(randomNumber);
 		$("#randomNumber").html(randomNumber);
 	}
 
-	console.log(crystalValue1);
-	console.log(crystalValue2);
-	console.log(crystalValue3);
-	console.log(crystalValue4);
-	console.log(totalCrystalValue);
+	$("#wins").html("Wins: " + wins);
+	$("#losses").html("Losses: " + losses);
+	$("#totalCrystalValue").html(score);
+
+	function reset() {
+		score = 0;
+		randomNumber = Math.floor(Math.random() * 120) + 19;
+		$("#randomNumber").html(randomNumber);
+		crystalValue1 = Math.floor(Math.random() * 12) + 1;
+		crystalValue2 = Math.floor(Math.random() * 12) + 1;
+		crystalValue3 = Math.floor(Math.random() * 12) + 1;
+		crystalValue4 = Math.floor(Math.random() * 12) + 1;
+		$("#totalCrystalValue").html(score);
+	};
+
 
 	$("#btn1").click(function() {
-		console.log("check");
-		$("#totalCrystalValue").html(totalCrystalValue);
+		score = score + crystalValue1
+		$("#totalCrystalValue").html(score);
+			if (score === randomNumber) {
+				wins = wins + 1;
+				reset();
+			}
+
+			if (score > randomNumber) {
+				losses = losses + 1;
+				reset();
+			}
+		$("#wins").html("Wins: " + wins);
+		$("#losses").html("Losses: " + losses);
 	});
 
 	$("#btn2").click(function() {
-		console.log("check");
-		$("#totalCrystalValue").html(totalCrystalValue);
+		score = score + crystalValue2
+		$("#totalCrystalValue").html(score);
+			if (score === randomNumber) {
+				wins = wins + 1;
+				reset();
+			}
+
+			if (score > randomNumber) {
+				losses = losses + 1;
+				reset();
+			}
+		$("#wins").html("Wins: " + wins);
+		$("#losses").html("Losses: " + losses);
 	});
 
 	$("#btn3").click(function() {
-		console.log("check");
-		$("#totalCrystalValue").html(totalCrystalValue);
+		score = score + crystalValue3
+		$("#totalCrystalValue").html(score);
+			if (score === randomNumber) {
+				wins = wins + 1;
+				reset();
+			}
+
+			if (score > randomNumber) {
+				losses = losses + 1;
+				reset();
+			}
+		$("#wins").html("Wins: " + wins);
+		$("#losses").html("Losses: " + losses);
 	});
 
 	$("#btn4").click(function() {
-		console.log("check");
-		$("#totalCrystalValue").html(totalCrystalValue);
+		score = score + crystalValue4
+		$("#totalCrystalValue").html(score);
+			if (score === randomNumber) {
+				wins = wins + 1;
+				reset();
+			}
+
+			if (score > randomNumber) {
+				losses = losses + 1;
+				reset();
+			}
+		$("#wins").html("Wins: " + wins);
+		$("#losses").html("Losses: " + losses);
 	});
 
 
 });
 
-	// The player will be shown a random number at the start of the game.
-
-	// When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
-
-	// Your game will hide this amount until the player clicks a crystal.
-
-	// When they do click one, update the player's score counter.
 	
 	// The player wins if their total score matches the random number from the beginning of the game.
 
