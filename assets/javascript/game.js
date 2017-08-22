15f9979aa7161d18d9feb2cd3ddec1edb9931853
Devon Worland <devonworland@Devon-Worlands-MacBook-Pro.local>
2017-08-21 19:10:48 -0500

    remove psuedo code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -97,15 +97,3 @@ $(document).ready(function() {
 });
 
 	
-	// The player wins if their total score matches the random number from the beginning of the game.
-
-	// The player loses if their score goes above the random number.
-
-	// The game restarts whenever the player wins or loses.
-
-	// When the game begins again, the player should see a new random number. 
-	//Also, all the crystals will have four new hidden values. 
-	//Of course, the user's score (and score counter) will reset to zero.
-
-	//The app should show the number of games the player wins and loses. 
-	//To that end, do not refresh the page as a means to restart the game.
